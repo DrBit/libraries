b972627808a145c1570042ea33c99a2215ebaab3
Quim <betamaster50@gmail.com>
2012-06-14 13:01:16 +0200

    Updated DB and Stopwatch libraries
    Updated DB and Stopwatch libraries to work with UNO version
    Deleted Ethernet unused libraries

--- StopWatch/StopWatch.cpp
+++ StopWatch/StopWatch.cpp
@@ -16,7 +16,11 @@
 //
 
 #include "StopWatch.h"
-#include "wiring.h"
+#if defined(ARDUINO) && ARDUINO >= 100
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+#endif
 
 StopWatch::StopWatch()
 {
